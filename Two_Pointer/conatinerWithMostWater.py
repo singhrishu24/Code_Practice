8905808b44d9716972b511423a3f63ba940c72be
Rishu <500061292@upesalumni.upes.ac.in>
2023-07-28 02:51:58 +0530

    PRCS_3

---
+++ Two_Pointer/conatinerWithMostWater.py
@@ -0,0 +1,19 @@
+'''
+Conatiner with most water.
+'''
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height)-1
+        res  = 0
+
+        while l < r:
+            h = min(height[l], height[r])
+            w = r - l
+            res = max(res, h * w)
+            if height[l] < height[r]:
+                l += 1
+            elif height[r] <= height[l]:
+                r -= 1
+        return res            
+
