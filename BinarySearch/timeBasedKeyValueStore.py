9994e22be175ff1043128246ccb026ef3e020e61
Rishu <500061292@upesalumni.upes.ac.in>
2023-07-31 21:58:21 +0530

    PRCS_6

---
+++ BinarySearch/timeBasedKeyValueStore.py
@@ -0,0 +1,27 @@
+'''
+Time Based Key Value store.
+'''
+class TimeMap:
+
+    def __init__(self):
+        self.store = {} # key : list of [val, timeStamp]
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        values = self.store.get(key, [])
+
+        # binary search
+        l, r = 0, len(values) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m +1
+            else:
+                r = m-1
+        return res                         
