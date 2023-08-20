8499fc1a0137cbe704eca872d0386f5144d9426b
Rishu <500061292@upesalumni.upes.ac.in>
2023-08-21 00:27:02 +0530

    PRCS_19

---
+++ Backtracking/permutation.py
@@ -0,0 +1,21 @@
+'''
+Permutations
+'''
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        # base case 
+        if len(nums) == 1:
+            return [nums[:]] # deep copy
+        
+        for i in range (len(nums)):
+            n  = nums.pop(0)
+            perms = self.permute(nums)
+
+
+            for perm in perms:
+                perm.append(n)
+            res.extends(perms)
+            nums.append(n)
+        return res        
