b50ba2f143c59b8accd957cfca93363ada1a3c94
Rishu <500061292@upesalumni.upes.ac.in>
2023-09-05 15:53:37 +0530

    PRCS_26

---
+++ Backtracking/finRedundantConnection.py
@@ -0,0 +1,34 @@
+'''
+Redundant Connection : Turn the graph to tree
+Using union find algorithm 
+Union : merge/connecct  find : check the parent of the node 
+'''
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        par = [i for i in range(len(edges) + 1)]
+        rank = [1] * (len(edges)+1)
+
+        # define find function
+        def find(n):
+            p = par[n]
+            while p != par[p]:
+                par[p] = par[par[p]]
+                p = par[p]
+            return p   
+        # define union function 
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return False
+            if rank[p1] > rank[p2]:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            return True        
+        # function call
+        for n1, n2 in edges:
+            if not union(n1, n2):
+                return[n1, n2] 
